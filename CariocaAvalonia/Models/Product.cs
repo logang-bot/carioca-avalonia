b1c5dac820951906b3c20407f3361bad90d9de30
Gilgamesh555 <painarniano@gmail.com>
2025-05-11 14:20:44 -0400

    add models

---
+++ CariocaAvalonia/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using CariocaAvalonia.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CariocaAvalonia.Models
+{
+    class Product
+    {
+        protected Guid Id { get; }
+
+        protected string Name { get; }
+
+        protected string Description { get; }
+
+        protected double Cost { get; }
+
+        public List<Fittings> Fittings { get; set; }
+
+        public Product(Guid id, string name, string description, double cost, List<Fittings> fittings)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Cost = cost;
+            Fittings = fittings;
+        }
+    }
+}
